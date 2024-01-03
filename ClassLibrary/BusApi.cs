b819387901f05137e335e079fd0a6cb570c4fc89
Cfournymanassevitz <carole.fourny@WJS-06>
2024-01-03 16:21:28 +0100

    refacto pas fini

---
+++ ClassLibrary/BusApi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using CollecteDonnees;
+using Newtonsoft.Json;
+
+namespace ClassLibrary
+{
+    public class RequestResponse
+    {
+
+        public string DoRequest(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            return responseFromServer;
+        }
+        public List<LineData> GetLine()
+        {
+            string urlBus = "https://data.mobilites-m.fr/api/linesNear/json?x=5.73119705178461&y=45.184446886268645&dist=400&details=true";
+            string json = DoRequest(urlBus);
+            List<LineData> line = JsonConvert.DeserializeObject<List<LineData>>(json);
+            return line;
+
+        }
+
+    }
+}
--- CollecteDonnees/Program.cs
+++ CollecteDonnees/Program.cs
@@ -10,16 +10,7 @@ namespace CollecteDonnees
     {
         static void Main(string[] args)
         {
-            // ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-            // List<LineData> busTramEtc = new List<LineData>();
-            // WebResponse ClassLibrary = new WebResponse();
-           
-
-            // foreach ( LineData lignebus in bus)
-            // {
-            //     Console.WriteLine($"linedata {lignebus.name} , {lignebus.zone} , {lignebus.lon},{lignebus.id} ,{lignebus.}" );
-            // }
-
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
             RequestResponse response = new RequestResponse();
             List<LineData> bus = response.GetLine();
             foreach (LineData lignebus in bus)
