18f8e8d7ac3bc30f9094806e6e36c1684c5c2995
Cfournymanassevitz <carole.fourny@WJS-06>
2024-01-04 10:40:35 +0100

    test

--- TestBusApi/UnitTest1.cs
+++ TestBusApi/UnitTest1.cs
@@ -16,7 +16,17 @@ namespace TestBusApi
             BusApi fake = new BusApi(request);
             List<LineData> result = fake.GetLine();
             Assert.AreEqual("Grenoble, Champs-Elysées", result[0].name);
+            Assert.AreEqual("SEM:0844", result[0].id);
+            Assert.AreEqual("SEM:0844", result[0].id);
 
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IRequest request = new FakeRequest();
+            BusApi fake = new BusApi(request);
+            List<LineData> result2 = fake.GetLine();
+            Assert.AreEqual("25", result2[0].id);
         }
     }
 }
